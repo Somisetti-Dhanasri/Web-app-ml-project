8d1cc439bd43b10f40e03a89e6aef51a3744d449
Somisetti-Dhanasri <155796060+Somisetti-Dhanasri@users.noreply.github.com>
2024-01-06 11:43:33 +0530

    create ml model

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from sklearn.linear_model import LogisticRegression
 import streamlit as st
 
 dataset=pd.read_csv('crop.csv')
-X=dataset.iloc(:,:-1).values
-Y=dataset.iloc(:,-1).values
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
 
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 
